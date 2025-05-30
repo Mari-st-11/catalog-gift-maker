44e64fc9820a2b3076dfe8fbf01ae930c850b24e
Mari-st-11 <tiptoe.nov@gmail.com>
2025-05-31 07:52:50 +0900

    update: rubocopエラー修正

--- app/decorators/gift_list_decorator.rb
+++ app/decorators/gift_list_decorator.rb
@@ -1,6 +1,6 @@
 class GiftListDecorator < Draper::Decorator
   delegate_all
-# 切り出してみたけど使わないので後で消す
+  # 切り出してみたけど使わないので後で消す
   def giftlist_title
     "#{object.recipient_name}さんへ贈る#{object.purpose.presence || 'ギフトリスト'}"
   end
--- app/models/gift_list.rb
+++ app/models/gift_list.rb
@@ -3,7 +3,7 @@ class GiftList < ApplicationRecord
   validates :purpose, length: { maximum: 255 }
   enum cover_image: { default: 0, birthday: 1 }
   validates :public_name, length: { maximum: 255 }
-  validates :is_public, inclusion: [true, false]
+  validates :is_public, inclusion: [ true, false ]
 
   belongs_to :user
 end
