777b904a2272fe4148d65c7994c3e9c1b9872cd1
Mari <tiptoe.nov@gmail.com>
2025-06-02 23:43:47 +0900

    add: ギフトリスト登録画面の作成 (#64)
    * add: ギフトリスト登録画面の作成
    
    * update: ギフトリストの作成機能
    
    * fix: メールバリデーションの修正

--- app/controllers/gift_lists_controller.rb
+++ app/controllers/gift_lists_controller.rb
@@ -1,5 +1,23 @@
 class GiftListsController < ApplicationController
   def index
-    @gift_lists = GiftList.includes(:user)
+    @gift_lists = current_user.gift_lists.includes(:user)
+  end
+
+  def new
+    @gift_list = GiftList.new
+  end
+
+  def create
+    @gift_list = current_user.gift_lists.build(gift_lists_params)
+
+    if @gift_list.save
+      redirect_to gift_lists_path(@gift_list)
+    end
+  end
+
+  private
+
+  def gift_lists_params
+    params.require(:gift_list).permit(:recipient_name, :purpose)
   end
 end
