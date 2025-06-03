e530bf8a0db45e9cfe04581e03be2e4515eaf89c
Mari-st-11 <tiptoe.nov@gmail.com>
2025-06-04 08:26:14 +0900

    update:バリデーションの設定

--- app/models/gift_item.rb
+++ app/models/gift_item.rb
@@ -1,2 +1,10 @@
 class GiftItem < ApplicationRecord
+  validates: url, length: { maximum: 535 }
+  validates: name, length: { maximum: 225 }
+  validates: description, length: { maximum: 225 }
+  validate: status
+  enum status: { unchoice: 0, choice: 1 }
+
+  belongs_to :user
+  belongs_to :gift_list
 end
