efaf4630ea6909a6bde1f80a06ea9809a8098071
Mari-st-11 <tiptoe.nov@gmail.com>
2025-06-30 16:43:44 +0900

    update: ACL無効の設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,10 @@ CarrierWave.configure do |config|
     config.fog_directory  = "cgm-production-images"
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], # 環境変数
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # 環境変数
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: "ap-northeast-1",
-      path_style: true
+      path_style: true,
     }
+    config.fog_public = false
 end
