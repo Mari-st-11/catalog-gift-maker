6c5e7c809e2818fdea98fdea363803a2e836915c
Mari-st-11 <tiptoe.nov@gmail.com>
2025-07-05 02:24:09 +0900

    update: 商品URL入力フォームをurl_fieldに変更

--- app/models/gift_item.rb
+++ app/models/gift_item.rb
@@ -1,5 +1,5 @@
 class GiftItem < ApplicationRecord
-  validates :url, presence: true, unless: :name?
+  validates :url, presence: true, unless: :name?, format: /\A#{URI::regexp(%w(http https))}\z/
   validates :name, length: { maximum: 225 }, presence: true, unless: :url?
   enum :status, { unselected: 0, selected: 1 }
   mount_uploader :image, ImageUploader
