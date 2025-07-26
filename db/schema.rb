37a5c216e68860406452c1dc32ad6755957058c6
Mari-st-11 <tiptoe.nov@gmail.com>
2025-07-27 06:13:19 +0900

    add:messageカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_02_080354) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_26_210858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_02_080354) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "message"
     t.index ["user_id"], name: "index_gift_lists_on_user_id"
     t.index ["uuid"], name: "index_gift_lists_on_uuid", unique: true
   end
