7a975ddb999e38e10e83b6db0585b913ca200a77
Mari-st-11 <tiptoe.nov@gmail.com>
2025-08-01 01:21:39 +0900

    update: ギフトリスト作成時のバリデーションエラーメッセージを表示

--- app/controllers/gift_lists_controller.rb
+++ app/controllers/gift_lists_controller.rb
@@ -26,6 +26,8 @@ class GiftListsController < ApplicationController
   def update
     if @gift_list.update(gift_list_params)
       redirect_to gift_list_path(@gift_list)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
